053d31aa9e776527a5711792de27fc1f1faa8677
Federico Miguez <fmiguez@taxsynapse.com>
2021-04-28 01:17:07 -0300

    valdiacion para que ingrese un pokemon

--- store/actions.js
+++ store/actions.js
@@ -1,13 +1,17 @@
 export default {
   async getPokemon({commit}, id){
-    commit("SET_LOADING", true)
-    await this.$axios.get(`/pokemon/${id}`).then(response => {
-      commit("SET_DATA", response.data)
-      commit("SET_HAS_DATA", true)
-    })
-    .catch(error => {
-      commit("SET_HAS_DATA", false)
-    });
-    commit("SET_LOADING", false)
+    if(!id){
+      commit("SET_HAS_DATA", null)
+    }else{
+      commit("SET_LOADING", true)
+      await this.$axios.get(`/pokemon/${id}`).then(response => {
+        commit("SET_DATA", response.data)
+        commit("SET_HAS_DATA", true)
+      })
+      .catch(error => {
+        commit("SET_HAS_DATA", false)
+      });
+      commit("SET_LOADING", false)
+    }
   }
 }
